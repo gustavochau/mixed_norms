2c84da5312c1e5e08d2f3ed20b749c1c328114b6
Gustavo Chau <gchau@stanford.edu>
2017-09-04 17:43:18 -0500

    compara

--- Problem2_inf-1/search_linf1_comp.m
+++ Problem2_inf-1/search_linf1_comp.m
@@ -2,6 +2,9 @@ clc;
 clear;
 close all;
 
+
+
+
 N = 500;
 M = 100;
 % rng(3);
@@ -13,54 +16,56 @@ num_real = 100;
 errores = zeros(num_real,2);
 tiempo = zeros(num_real,2);
 
-for zz=1:num_real
-%     rng(5*zz)
-    zz
-    
-    B = (rand(N,M)-0.5);
-    lambda =0.1;
-
-%     tic
-%     norma_B = max(sum(abs(B),2));
-%     [~,ib]=max(sum(abs(B),2));
-%     tau_1 = norm(shrink(B(ib,:),lambda),1);
-%     [ A_stef, tau_opt_stef,iter] = solve_l1_search_steffenson( B,lambda, tau_1);
-%     X_stef = B-A_stef;
-%     tiempo(zz,2) = toc;
-%     errores(zz,2) = abs(compute_mixed_norm(X_stef,1,inf)-lambda);
-%     iter_num(zz,2) = iter;
-%     clear X_stef A_stef;
-    
-    tic
-    norma_B = max(sum(abs(B),2));
-    [~,ib]=max(sum(abs(B),2));
-    tau_1 = norm(shrink(B(ib,:),lambda),1);
-    [ A_newt, tau_opt_stef,iter] = solve_l1_search_newton( B,lambda, tau_1);
-    X_newt = B-A_newt;
-    tiempo(zz,2) = toc;
-    errores(zz,2) = abs(compute_mixed_norm(X_newt,1,inf)-lambda);
-    iter_num(zz,2) = iter;
-    clear X_newt A_newt;
-    
-    tic
-    norma_B = max(sum(abs(B),2));
-    [~,ib]=max(sum(abs(B),2));
-    tau_1 = norm(shrink(B(ib,:),lambda),1);
-    [ A_newt_prun, tau_opt_stef,iter] = solve_l1_search_newton_pruned( B,lambda, tau_1);
-    X_newt_prun = B-A_newt_prun;
-    tiempo(zz,3) = toc;
-    errores(zz,3) = abs(compute_mixed_norm(X_newt_prun,1,inf)-lambda);
-    iter_num(zz,3) = iter;
-    clear X_newt_prun A_newt_prun;    
-    
-   
-    tic
-    [ X_sra, theta_opt, iter ] = solve_sra(B,lambda);
-    tiempo(zz,1) = toc;
-    errores(zz,1) =abs(compute_mixed_norm(X_sra,1,inf)-lambda);
-    iter_num(zz,1) = iter;
-    clear X_sra
+for lambda =[0.05,0.1,0.2];
+    lambda
+    for zz=1:num_real
+        %     rng(5*zz)
+        disp(num2str(zz))
+        
+        B = (rand(N,M)-0.5);
+        
+        %     tic
+        %     norma_B = max(sum(abs(B),2));
+        %     [~,ib]=max(sum(abs(B),2));
+        %     tau_1 = norm(shrink(B(ib,:),lambda),1);
+        %     [ A_stef, tau_opt_stef,iter] = solve_l1_search_steffenson( B,lambda, tau_1);
+        %     X_stef = B-A_stef;
+        %     tiempo(zz,2) = toc;
+        %     errores(zz,2) = abs(compute_mixed_norm(X_stef,1,inf)-lambda);
+        %     iter_num(zz,2) = iter;
+        %     clear X_stef A_stef;
+                
+        tic
+        [ X_sra, theta_opt, iter ] = solve_sra(B,lambda);
+        tiempo(zz,1) = toc;
+        errores(zz,1) =abs(compute_mixed_norm(X_sra,1,inf)-lambda);
+        iter_num(zz,1) = iter;
+        clear X_sra
+        
+        tic
+        norma_B = max(sum(abs(B),2));
+        [~,ib]=max(sum(abs(B),2));
+        tau_1 = norm(shrink(B(ib,:),lambda),1);
+        [ A_newt, tau_opt_stef,iter] = solve_l1_search_newton( B,lambda, tau_1);
+        X_newt = B-A_newt;
+        tiempo(zz,2) = toc;
+        errores(zz,2) = abs(compute_mixed_norm(X_newt,1,inf)-lambda);
+        iter_num(zz,2) = iter;
+        clear X_newt A_newt;
+        
+        tic
+        norma_B = max(sum(abs(B),2));
+        [~,ib]=max(sum(abs(B),2));
+        tau_1 = norm(shrink(B(ib,:),lambda),1);
+        [ A_newt_prun, tau_opt_stef,iter] = solve_l1_search_newton_pruned( B,lambda, tau_1);
+        X_newt_prun = B-A_newt_prun;
+        tiempo(zz,3) = toc;
+        errores(zz,3) = abs(compute_mixed_norm(X_newt_prun,1,inf)-lambda);
+        iter_num(zz,3) = iter;
+        clear X_newt_prun A_newt_prun;
+        
+    end
+    resumen = [mean(errores)' mean(iter_num)' mean(tiempo)'];
+    save(['results_' num2str(N) 'x' num2str(M) '_lambda' num2str(lambda) '.mat'],'iter_num','errores','tiempo','resumen')
 
 end
-
-save(['results_' num2str(N) 'x' num2str(M) '_lambda' num2str(lambda) '.mat'],'iter_num','errores','tiempo')
