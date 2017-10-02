3bff0f9ab5e65df5a06863313066fb89524ae0e6
Gustavo Chau <gchau@stanford.edu>
2017-10-02 18:20:03 -0500

    server

--- Problem2_inf-1/applications/fmri/fmri_MTL_bound.m
+++ Problem2_inf-1/applications/fmri/fmri_MTL_bound.m
@@ -34,7 +34,7 @@ end
 W0 = 0*(rand(M,K)-0.5);
 mem=0;
 alpha=0.01;
-max_iter=100;
+max_iter=300;
 gamma= 0.01;
 tau = 2.5;
 costo = @(U) costo_func(U,X,Y);
@@ -47,18 +47,18 @@ dual_norm = @(U) compute_mixed_norm(U,Inf,1);
 [W, num_iter ,hist,tiempo,hist_tau,cambio,h_bound,nw] = proj_grad_desc( W0, proj_op, grad_op, 1E-6, max_iter,alpha, 0, dual_norm);
 [W_mem, ~ ,~,tiempo_mem,hist_tau_mem] = proj_grad_desc( W0, proj_op, grad_op, 1E-6, max_iter,alpha, 1, dual_norm);
 
-
-figure
-plot(cambio)
-xlabel('# iteration k')
-ylabel('||W_{(k)}-W_{(k-1)}||_{\infty,1}')
-
-figure
-semilogy(hist_tau_mem)
-hold on
-semilogy(0.99*h_bound)
-% semilogy(ng*alpha)
-% semilogy(nw)
-legend('tau','lower bound')
-xlabel('# iteration k')
+% 
+% figure
+% plot(cambio)
+% xlabel('# iteration k')
+% ylabel('||W_{(k)}-W_{(k-1)}||_{\infty,1}')
+% 
+% figure
+% semilogy(hist_tau_mem)
+% hold on
+% semilogy(0.99*h_bound)
+% % semilogy(ng*alpha)
+% % semilogy(nw)
+% legend('tau','lower bound')
+% xlabel('# iteration k')
 
