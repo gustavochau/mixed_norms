aa453ef468d317c1e66737c6236760bc1cd9e7a9
Gustavo Chau <gchau@stanford.edu>
2017-10-15 22:07:32 -0500

    initial codes l0inf via l1inf

---
+++ l0inf/prueba_Xupdate.m
@@ -0,0 +1,20 @@
+clc;
+clear;
+close all;
+
+N = 5;
+M = 2;
+B = rand(N,M);
+X = rand(N,M);
+Z = rand(N,M);
+
+W = 1./X;
+W(abs(X)<1E-6) = 0;
+
+rho=1;
+cvx_begin
+    variable X_cvx(N,M)
+    minimize  (0.5*sum_square(X_cvx(:)-B(:)) +0.5*rho* sum_square(vec(W.*X_cvx-Z)))
+cvx_end
+
+X_grad = (rho*W.*Z+B)./(1+rho*W.*W);
